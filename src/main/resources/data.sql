35347e34f42c2f7b4cb19d07975c8d883a3cde9b
SkrNLALM <cakmakskr@gmail.com>
2023-05-04 00:00:04 +0200

    new data inserted

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,3 +7,5 @@ INSERT INTO users(first_name,last_name,user_name, pass_word,enabled, phone, gend
 VALUES ('Adam', 'Hijk','AH', '1234','True', '125487455','Male');
 INSERT INTO users(first_name,last_name,user_name, pass_word,enabled, phone, gender)
 VALUES ('Sam', 'Toe','ST', '12334','False', '1456487455','Female');
+INSERT INTO users(first_name,last_name,user_name, pass_word,enabled, phone, gender)
+VALUES ('Nihan', 'Cakmak','NC', '334','False', '145611455','Female');
